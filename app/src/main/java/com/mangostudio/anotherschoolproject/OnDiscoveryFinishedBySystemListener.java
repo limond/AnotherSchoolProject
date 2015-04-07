0a4a7beab422cb05027bc85c154f32b6508ccaf0
limond <leonkaucher@gmail.com>
2015-04-08 01:26:27 +0200

    Auf UTF-8 umgestellt, um Probleme beim Build-Vorgang zu lösen (Kommentare enthalten Umlaute)

--- app/src/main/java/com/mangostudio/anotherschoolproject/OnDiscoveryFinishedBySystemListener.java
+++ app/src/main/java/com/mangostudio/anotherschoolproject/OnDiscoveryFinishedBySystemListener.java
@@ -4,6 +4,6 @@ package com.mangostudio.anotherschoolproject;
  * Created by Leon on 06.04.2015.
  */
 public interface OnDiscoveryFinishedBySystemListener {
-    //Listener wird benutzt, um das Beenden der Ger‰te-Suche durch das System bekannt zu machen
+    //Listener wird benutzt, um das Beenden der Ger√§te-Suche durch das System bekannt zu machen
     void onFinished();
 }
--- app/src/main/java/com/mangostudio/anotherschoolproject/OnDiscoveryStatusChangeRequestListener.java
+++ app/src/main/java/com/mangostudio/anotherschoolproject/OnDiscoveryStatusChangeRequestListener.java
@@ -4,6 +4,6 @@ package com.mangostudio.anotherschoolproject;
  * Created by Leon on 03.04.2015.
  */
 public interface OnDiscoveryStatusChangeRequestListener {
-    //Listener wird benutzt, um bekannt zu machen, dass der Nutzer den Start oder den Stopp der Geräte-Suche wünscht
+    //Listener wird benutzt, um bekannt zu machen, dass der Nutzer den Start oder den Stopp der GerÃ¤te-Suche wÃ¼nscht
     void onStatusChange(int status);
 }
